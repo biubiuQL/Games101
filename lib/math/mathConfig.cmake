cd05ab4b08838462aac40309fc3804abad7433b2
DESKTOP-3FFMDEV\admin <2229608151@qq.com>
2024-09-11 13:42:33 +0800

    修改加载方式·

--- lib/math/mathConfig.cmake
+++ lib/math/mathConfig.cmake
@@ -1,1 +1,12 @@
-target_include_directories(math PUBLIC include)
+file(GLOB SOURCES ${CMAKE_CURRENT_LIST_DIR}/src/*.cpp)
+add_library(math STATIC IMPORTED)
+
+
+
+set_target_properties(math PROPERTIES IMPORTED_LOCATION ${CMAKE_CURRENT_LIST_DIR}/build/libmath.a)
+
+# add_library(math STATIC ${CMAKE_CURRENT_LIST_DIR}/build)
+# target_link_directories(math PUBLIC ${CMAKE_CURRENT_LIST_DIR}/build)
+target_include_directories(math PUBLIC  ${CMAKE_CURRENT_LIST_DIR}/include)
+
+set(math_libs math)
