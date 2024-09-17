1248a474788aa23db238e1219c7597f00a6b3dd4
DESKTOP-34FVBVG\lirong <2229608151@qq.com>
2024-09-17 16:43:50 +0800

    提交

--- lib/math/mathConfig.cmake
+++ lib/math/mathConfig.cmake
@@ -1,12 +1,19 @@
-file(GLOB SOURCES ${CMAKE_CURRENT_LIST_DIR}/src/*.cpp)
+
+
+if(EXISTS ${CMAKE_CURRENT_LIST_DIR}/build/libmath.a)
+
 add_library(math STATIC IMPORTED)
-
-
 
 set_target_properties(math PROPERTIES IMPORTED_LOCATION ${CMAKE_CURRENT_LIST_DIR}/build/libmath.a)
 
-# add_library(math STATIC ${CMAKE_CURRENT_LIST_DIR}/build)
-# target_link_directories(math PUBLIC ${CMAKE_CURRENT_LIST_DIR}/build)
-target_include_directories(math PUBLIC  ${CMAKE_CURRENT_LIST_DIR}/include)
+else()
+
+file(GLOB SOURCES ${CMAKE_CURRENT_LIST_DIR}/src/*.cpp)
+
+add_library(math STATIC ${SOURCES})
+
+endif()
+target_include_directories(math INTERFACE  ${CMAKE_CURRENT_LIST_DIR}/include)
+target_link_directories(math INTERFACE ${CMAKE_CURRENT_LIST_DIR}/build)
 
 set(math_libs math)
